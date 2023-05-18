f951306a92535e3211f90d872b695d88bccf57a0
Efren Fuentes <developer@efrenfuentes.net>
2023-05-18 17:08:03 -0400

    update mix information

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule HaversineEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "haversineEx",
+      source_url: "https://github.com/efrenfuentes/haversineEX"
     ]
   end
 
@@ -21,5 +25,18 @@ defmodule HaversineEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     []
+  end
+
+  defp description() do
+    "Provides some helpers functions to calculate the distance between two points on Earth using the Haversine formula. Also can find the bearing between two points, and get a point at a given distance and bearing from a given point."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "haversineEx",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/efrenfuentes/haversineEX"},
+    ]
   end
 end
